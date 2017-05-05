ba51030aff87a70baffae6b1d79138fb7718a0d1
michael <michael.pc168@gmail.com>
2017-05-05 12:27:27 +1000

    anotherupdate

--- botchan.js
+++ botchan.js
@@ -74,7 +74,7 @@ client.on("message", message => {
 client.login(config.token);
 
 //
-
+/*
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
@@ -87,6 +87,22 @@ rl.question("", function(answer) {
         console.log("bye");
         client.destroy();
    }
+});*/
+
+
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
+
+
 
 //git push -u origin master
