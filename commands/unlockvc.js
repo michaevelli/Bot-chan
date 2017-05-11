18d2b03ee5c4fe596e95023b665a6003dbbd85e7
michael <michael.pc168@gmail.com>
2017-05-11 22:13:15 +1000

    removed redundent commands

--- commands/unlockvc.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
-
-    const channel = message.member.voiceChannel;
-
-    channel.overwritePermissions(message.guild.roles.find("name", "@everyone"), {'CONNECT': true});
-
-    console.log(message.author.id + " ran unlockvc on " + message.channel.id);
-}
