c689731d1726917d59b6b5510efdfad2c63794e6
michael <michael.pc168@gmail.com>
2017-06-14 19:54:41 +1000

    try

--- commands/clean.js
+++ commands/clean.js
@@ -38,7 +38,7 @@ exports.run = (client, message, args) => {
     }
     for(i = 0; i < n; i++){
         if(message.channel.lastMessageID != undefined){
-            message.channel.fetchMessage(message.channel.lastMessageID).then(mess => {
+            message.channel.fetchMessages({limit: 1}).then(mess => {
                 console.log(mess.delete());
             });
         } else {
