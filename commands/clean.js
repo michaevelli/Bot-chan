892909d6510946adedd025a049b131e81539f179
michael <michael.pc168@gmail.com>
2017-06-14 20:23:50 +1000

    try

--- commands/clean.js
+++ commands/clean.js
@@ -10,15 +10,16 @@ exports.run = (client, message, args) => {
     if(args[0] == parseInt(args[0])){
         n = args[0] + 1;
     }
+    trigger = 0;
 
-    while(n > 100){
-        message.channel.bulkDelete(100).catch(goto finish);
+    while(n > 100 && trigger == 0){
+        message.channel.bulkDelete(100).catch(trigger = 1;);
         n -= 100;
     }
-    message.channel.bulkDelete(n);
+    if(trigger == 0){
+        message.channel.bulkDelete(n);
+    }
 
-
-    [lbl] finish:
     message.channel.send("> Cleaned!").catch(console.error);
     console.log(message.author.id + " ran clean on " + message.channel.id + " | param: " + args[0]);
 };
