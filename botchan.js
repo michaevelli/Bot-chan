38073ba85629314b368f629aadaec3537c834493
michael <michael.pc168@gmail.com>
2017-06-14 22:21:14 +1000

    tests

--- botchan.js
+++ botchan.js
@@ -74,8 +74,8 @@ client.login(config.token);
 var interval = setInterval (function(){
     //role = message.guild.roles.get("324502111188418561");
     role = client.guilds.get("265767904065224717").roles.get("324502111188418561");
-    role.setColor(changeHue(role.hexColor, 50));
-}, 1000)
+    role.setColor(changeHue(role.hexColor, 1));
+}, 1)
 
 //
 /*
