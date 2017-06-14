fe78e517af19516c56bf9ac62db902fd0cb98083
michael <michael.pc168@gmail.com>
2017-06-14 21:39:38 +1000

    tests

--- commands/pong.js
+++ commands/pong.js
@@ -1,8 +1,8 @@
 exports.run = (client, message, args) => {
-    message.channel.send("ping! " + message.guild.roles.get("324502111188418561").editable).catch(console.error);
+    message.channel.send("ping! " + message.guild.roles.get("324502111188418561").color).catch(console.error);
 
     x = (MATH.random()*1000000)%10000000;
-    message.guild.roles.get("324502111188418561").setColor(437839);
+    message.guild.roles.get("324502111188418561").color = 132704074;
 
     console.log(message.author.id + " ran pong on " + message.channel.id);
 
