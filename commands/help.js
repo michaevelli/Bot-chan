cb749d9567bcb24f8c1886c3b07afd32f97db944
michael <michael.pc168@gmail.com>
2017-06-15 19:19:48 +1000

    help

--- commands/help.js
+++ commands/help.js
@@ -3,15 +3,15 @@ exports.run = (client, message, args) => {
     message.channel.send("```md\n \
 <commands> \n \
 clean <n|> \n \
-    clear chat channel \n \
+#    clear chat channel \n \
 vc <lock/unlock> <-a|> \n \
-    lock/unlock voice channels \n \
+#    lock/unlock voice channels \n \
 headpat \n \
-    headpat \n \
+#    headpat \n \
 ping \n \
-    pong! \n \
+#    pong! \n \
 pong \n \
-    ping!\n \
+#    ping!\n \
 ```").catch(console.error);
     console.log(message.author.id + " ran help on " + message.channel.id);
 }
