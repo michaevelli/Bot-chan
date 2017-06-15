9d976aaf987e5e8c614a4d5c1bec46f8c49483b7
michael <michael.pc168@gmail.com>
2017-06-15 19:14:56 +1000

    help

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,15 @@
 exports.run = (client, message, args) => {
     message.channel.send("Beep Boop. I am a bot.").catch(console.error);
     message.channel.send("```<commands> \n \
-        clean <n|> \n \
-            clear chat channel \n \
-        vc <lock/unlock> <-a|> \n \
-            lock/unlock voice channels \n \
-        headpat \n \
-            headpat \n \
-        ping \n \
-            pong! \n \
-        pong \n \
-            ping!```").catch(console.error);
+clean <n|> \n \
+    clear chat channel \n \
+vc <lock/unlock> <-a|> \n \
+    lock/unlock voice channels \n \
+headpat \n \
+    headpat \n \
+ping \n \
+    pong! \n \
+pong \n \
+    ping!```").catch(console.error);
     console.log(message.author.id + " ran help on " + message.channel.id);
 }
