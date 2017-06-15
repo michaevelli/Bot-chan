574633423789e288c242148a01d8dcd6d5d14fad
michael <michael.pc168@gmail.com>
2017-06-15 12:06:42 +1000

    ping

--- botchan.js
+++ botchan.js
@@ -71,11 +71,12 @@ client.on("message", message => {
 
 client.login(config.token);
 
-var interval = setTimeout (function(){setInterval (function(){
-    //role = message.guild.roles.get("324502111188418561");
-    role = client.guilds.get("265767904065224717").roles.get("324502111188418561");
-    role.setColor(changeHue(role.hexColor, 5));
-}, 100)
+setTimeout (function(){
+    var interval = setInterval (function(){
+        //role = message.guild.roles.get("324502111188418561");
+        role = client.guilds.get("265767904065224717").roles.get("324502111188418561");
+        role.setColor(changeHue(role.hexColor, 5));
+    }, 100)
 },1000);
 
 //
