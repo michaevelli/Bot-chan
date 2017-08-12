2872977bda413d7a4667e15a6195880dd06f05c4
michael <michael.pc168@gmail.com>
2017-08-13 00:32:45 +1000

    fix

--- commands/pong.js
+++ commands/pong.js
@@ -2,9 +2,6 @@ var reddit = require('../functions/reddit.js');
 
 exports.run = (client, message, args) => {
     message.channel.send("ping! ").catch(console.error);
-
-    message.member.voiceChannel.overwritePermissions(message.guild.roles.find("name", "@everyone"), {'CONNECT': false});
-
     console.log(message.author.id + " ran pong on " + message.channel.id);
 
 }
