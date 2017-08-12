16eacb59e7fa4fa3c77f3ddcfeb549d7eccee8f1
michael <michael.pc168@gmail.com>
2017-08-12 13:36:52 +1000

    improved reddit function

--- commands/pong.js
+++ commands/pong.js
@@ -3,7 +3,7 @@ var reddit = require('../functions/reddit.js');
 exports.run = (client, message, args) => {
     message.channel.send("ping! ").catch(console.error);
 
-    reddit.reddithot(client, client.guilds.find("id","167895139518382081"));
+    //reddit.reddithot(client, client.guilds.find("id","167895139518382081"));
 
     console.log(message.author.id + " ran pong on " + message.channel.id);
 
